15a2409ded1e264fde3a33dc65167ae147388272
Low Jun En <lowjunen@gmail.com>
2018-04-03 22:40:34 +0800

    Added the monoco mode into the files mentioned below.

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -525,6 +525,8 @@ int commander_main(int argc, char *argv[])
 				new_main_state = commander_state_s::MAIN_STATE_AUTO_LAND;
 			} else if (!strcmp(argv[2], "auto:precland")) {
 				new_main_state = commander_state_s::MAIN_STATE_AUTO_PRECLAND;
+                        } else if (!strcmp(argv[2], "monocopter")) {
+                                new_main_state = commander_state_s::MAIN_STATE_MONOCO;
 			} else {
 				warnx("argument %s unsupported.", argv[2]);
 			}
@@ -538,6 +540,44 @@ int commander_main(int argc, char *argv[])
 			warnx("missing argument");
 		}
 	}
+
+        if (!strcmp(argv[1],"currentmode")) {
+                if (internal_state.main_state == 0) {
+                        PX4_INFO("current flight mode is manual");
+                } else if (internal_state.main_state == 1) {
+                        PX4_INFO("current flight mode is altitude control");
+                } else if (internal_state.main_state == 2) {
+                        PX4_INFO("current flight mode is position control");
+                } else if (internal_state.main_state == 3) {
+                        PX4_INFO("current flight mode is mission");
+                } else if (internal_state.main_state == 4) {
+                        PX4_INFO("current flight mode is loiter");
+                } else if (internal_state.main_state == 5) {
+                        PX4_INFO("current flight mode is rtl");
+                } else if (internal_state.main_state == 6) {
+                        PX4_INFO("current flight mode is acro");
+                } else if (internal_state.main_state == 7) {
+                        PX4_INFO("current flight mode is offboard");
+                } else if (internal_state.main_state == 8) {
+                        PX4_INFO("current flight mode is stabilized");
+                } else if (internal_state.main_state == 9) {
+                        PX4_INFO("current flight mode is rattitude");
+                } else if (internal_state.main_state == 10) {
+                        PX4_INFO("current flight mode is takeoff");
+                } else if (internal_state.main_state == 11) {
+                        PX4_INFO("current flight mode is land");
+                } else if (internal_state.main_state == 12) {
+                        PX4_INFO("current flight mode is follow");
+                } else if (internal_state.main_state == 13) {
+                        PX4_INFO("current flight mode is precland");
+                } else if (internal_state.main_state == 14) {
+                        PX4_INFO("current flight mode is max");
+                } else if (internal_state.main_state == 15) {
+                        PX4_INFO("current flight mode is monocopter");
+                }
+
+                return 0;
+        }
 
 	if (!strcmp(argv[1], "lockdown")) {
 
@@ -3570,7 +3610,8 @@ set_control_mode()
 
 	switch (status.nav_state) {
 	case vehicle_status_s::NAVIGATION_STATE_MANUAL:
-		control_mode.flag_control_manual_enabled = true;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = stabilization_required();
 		control_mode.flag_control_attitude_enabled = stabilization_required();
@@ -3584,7 +3625,8 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_STAB:
-		control_mode.flag_control_manual_enabled = true;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = true;
@@ -3598,7 +3640,8 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_RATTITUDE:
-		control_mode.flag_control_manual_enabled = true;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = true;
@@ -3612,6 +3655,7 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_ALTCTL:
+                control_mode.flag_control_monoco_enabled = false;
 		control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
@@ -3626,6 +3670,7 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_POSCTL:
+                control_mode.flag_control_monoco_enabled = false;
 		control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
@@ -3638,6 +3683,21 @@ set_control_mode()
 		control_mode.flag_control_acceleration_enabled = false;
 		control_mode.flag_control_termination_enabled = false;
 		break;
+
+        case vehicle_status_s::NAVIGATION_STATE_MONOCO:
+                control_mode.flag_control_monoco_enabled = true;
+                control_mode.flag_control_manual_enabled = false;
+                control_mode.flag_control_auto_enabled = false;
+                control_mode.flag_control_rates_enabled = false;
+                control_mode.flag_control_attitude_enabled = false;
+                control_mode.flag_control_rattitude_enabled = false;
+                control_mode.flag_control_altitude_enabled = false;
+                control_mode.flag_control_climb_rate_enabled = false;
+                control_mode.flag_control_position_enabled = false;
+                control_mode.flag_control_velocity_enabled = false;
+                control_mode.flag_control_acceleration_enabled = false;
+                control_mode.flag_control_termination_enabled = false;
+                break;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_RTL:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_RCRECOVER:
@@ -3652,6 +3712,7 @@ set_control_mode()
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_TAKEOFF:
+                control_mode.flag_control_monoco_enabled = false;
 		control_mode.flag_control_manual_enabled = false;
 		control_mode.flag_control_auto_enabled = true;
 		control_mode.flag_control_rates_enabled = true;
@@ -3666,6 +3727,7 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LANDGPSFAIL:
+                control_mode.flag_control_monoco_enabled = false;
 		control_mode.flag_control_manual_enabled = false;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
@@ -3680,7 +3742,8 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_ACRO:
-		control_mode.flag_control_manual_enabled = true;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = true;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = false;
@@ -3695,7 +3758,8 @@ set_control_mode()
 
 	case vehicle_status_s::NAVIGATION_STATE_DESCEND:
 		/* TODO: check if this makes sense */
-		control_mode.flag_control_manual_enabled = false;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = false;
 		control_mode.flag_control_auto_enabled = true;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = true;
@@ -3710,7 +3774,8 @@ set_control_mode()
 
 	case vehicle_status_s::NAVIGATION_STATE_TERMINATION:
 		/* disable all controllers on termination */
-		control_mode.flag_control_manual_enabled = false;
+                control_mode.flag_control_monoco_enabled = false;
+                control_mode.flag_control_manual_enabled = false;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_rates_enabled = false;
 		control_mode.flag_control_attitude_enabled = false;
@@ -3724,6 +3789,7 @@ set_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_OFFBOARD:
+                control_mode.flag_control_monoco_enabled = false;
 		control_mode.flag_control_manual_enabled = false;
 		control_mode.flag_control_auto_enabled = false;
 		control_mode.flag_control_offboard_enabled = true;
